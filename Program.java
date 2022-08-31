d9136deef60016dc5c9dec5865e13b32d3233429
Андрій Попов <popov@kpnu.km.ua>
2022-08-31 17:12:00 +0300

    new

--- Program.java
+++ Program.java
@@ -7,6 +7,6 @@ public class Program {
      * @param args Command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(args.length);
+        System.out.println(args.length+2);
     }
 } 
