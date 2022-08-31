f20de3c6e6489fbf56f722e2443860a444d4fc04
Андрій Попов <popov@kpnu.km.ua>
2022-08-31 17:09:53 +0300

    first

---
+++ Program.java
@@ -0,0 +1,12 @@
+/**
+ * Main class
+ */
+public class Program {
+    /**
+     * Main method
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(args.length);
+    }
+} 
